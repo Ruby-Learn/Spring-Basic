3abe250307deaa3561836994f03aa3b39e125afa
MisterRuby <ruby8700@naver.com>
2023-01-09 18:54:08 +0900

    docs : @ComponentScan, @Autowired 정리

--- src/main/java/ruby/springbasic/member/MemberServiceImpl.java
+++ src/main/java/ruby/springbasic/member/MemberServiceImpl.java
@@ -1,12 +1,13 @@
 package ruby.springbasic.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository;
-
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Override
     public void join(Member member) {
